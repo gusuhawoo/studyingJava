64cd2f66d6d399fbd6b049ec3241826e8799c89b
suha <gusuhawoo@gmail.com>
2023-04-10 15:44:51 +0900

    Add : First Commit

---
+++ src/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+  public static void main(String[] args) {
+    int kor, eng;
+    kor = 77;
+    eng = 87;
+    int total;
+    total = kor + eng;
+    System.out.println("Total Score : "+total);
+    System.out.println("Average : "+total / 2);
+  }
+}
